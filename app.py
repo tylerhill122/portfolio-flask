75e9750b65071a4b706ac21a5fc590a414e97805
Tyler Hill <tylergenehill122@gmail.com>
2023-02-17 18:10:19 -0500

    projects page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
